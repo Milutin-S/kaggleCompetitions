5a5b17ec3861c2d497708cd72d21a567ca8fc737
Milutin-S <studen.m98@gmail.com>
2023-05-14 15:22:18 +0200

    Update data script, add transforms and visualization

--- Digit_Recognizer/scripts/dataset_loader.py
+++ Digit_Recognizer/scripts/dataset_loader.py
@@ -1,7 +1,8 @@
-import os
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from pathlib import Path
-from globals import TRAIN_PATH
+from globals import TRAIN_PATH, transforms
 
 import torch
 from torch.utils.data import DataLoader, Dataset
@@ -18,17 +19,42 @@ class DigitDataset(Dataset):
     def __getitem__(self, index):
         data = self.dataset.iloc[index]
         label, data = data[0], data[1:]
+        data = np.reshape(np.array(data, dtype=np.float16), (28, 28))
         # data = data[1:]
 
         if self.transforms:
-            pass
+            data = transforms(data)
+
         return data, label
+
+def data_viz(data, label, tile:bool=False):
+    if tile:
+        num_samples = data.shape[0]
+        plt_row = num_samples//2
+        plt_col = num_samples - plt_row
+        for i in range(num_samples):
+            img = data[i].squeeze()
+            plt.subplot(plt_row, plt_col, i+1)
+            plt.title("Label: " + str(int(label[i])))
+            plt.imshow(img)
+        plt.tight_layout()
+    else:
+        img = data.squeeze()
+        plt.title("Label: " + str(int(label)))
+        plt.imshow(img)
+    plt.show()
     
 if __name__ == '__main__':
     # print(f"[INFO] Current working directory: {os.getcwdb()}")
     # set_cwdb = 'c:\\Users\\Studen\\Documents\\vscode_projects\\kaggleCompetitions\\'
     # print(f"[INFO] Changing working directory to: {set_cwdb}")
     # os.chdir(set_cwdb)
-    dataset = DigitDataset(TRAIN_PATH)
+    tarin_set = DigitDataset(TRAIN_PATH, transforms)
+    train_loader = DataLoader(tarin_set, batch_size=8, shuffle=False, num_workers=10)
 
-    print(dataset.__getitem__(0))
+    images, labels = next(iter(train_loader))
+    data_viz(data=images, label=labels, tile=True)
+    data_viz(data=images[0], label=labels[0])
+    print(images.shape)
+
+    # print(tarin_set.__getitem__(0))
