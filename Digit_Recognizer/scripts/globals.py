7c0d5a8a23161a3f023bdcc0251783e4dc2e0645
Milutin-S <studen.m98@gmail.com>
2023-05-14 15:22:44 +0200

    Add transforms normalize to globals

--- Digit_Recognizer/scripts/globals.py
+++ Digit_Recognizer/scripts/globals.py
@@ -1,4 +1,10 @@
 from pathlib import Path
+import torchvision.transforms as transforms
 
 TRAIN_PATH = Path('./Digit_Recognizer/Dataset/train.csv')
 TEST_PATH = Path('./Digit_Recognizer/Dataset/test.csv')
+
+transforms = transforms.Compose(
+    [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+    # Actual mean and standard deviation of MNIST dataset
+)
