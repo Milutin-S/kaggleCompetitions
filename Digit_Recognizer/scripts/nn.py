844c9dc94249e66cad1f022754a97e98d77c5da2
Milutin-S <studen.m98@gmail.com>
2023-05-18 20:42:21 +0200

    Update nn script, chage input to stack b&w images to RGB

--- Digit_Recognizer/scripts/nn.py
+++ Digit_Recognizer/scripts/nn.py
@@ -4,17 +4,17 @@ import torchvision.models as models
 from torchsummary import summary
 
 
-class Net(nn.Module):
+class Digit_Net(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
+        super(Digit_Net, self).__init__()
 
-        self.conv1 = nn.Conv2d(1, 64, kernel_size = 5, padding = 'same', bias = False)
+        # self.conv1 = nn.Conv2d(1, 64, kernel_size = 5, padding = 'same', bias = False)
 
-        resnet18 = models.resnet18(weights='ResNet18_Weights.DEFAULT')
+        resnet18 = models.resnet18(weights="ResNet18_Weights.DEFAULT")
         # print(resnet18)
-        self.resnet18_core = nn.Sequential(*(list(resnet18.children())[1:-1]))
-        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
-        self.dropout = nn.Dropout(p = 0.2)
+        self.resnet18_core = nn.Sequential(*(list(resnet18.children())[:-1]))
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.dropout = nn.Dropout(p=0.2)
 
         self.relu = nn.ReLU()
 
@@ -26,25 +26,23 @@ class Net(nn.Module):
         self.softmax = nn.Softmax(1)
 
     def forward(self, x):
-        x = self.conv1(x)
+        # x = self.conv1(x)
+        x = torch.cat([x, x, x], dim=1)
         x = self.resnet18_core(x)
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.dropout(x)
-        # x = self.fc1(x)
-        # x = self.fc2(x)
         x = self.relu(self.fc1(x))
         x = self.relu(self.fc2(x))
         x = self.relu(self.fc3(x))
         x = self.softmax(x)
 
-        return x 
+        return x
 
 
-if __name__ == '__main__':
-    model = Net()
+if __name__ == "__main__":
+    model = Digit_Net()
 
     print(model)
-    print('='*100)
+    print("=" * 100)
     summary(model.cuda(), (1, 28, 28))
-
