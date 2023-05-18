efa545c85db81a219d263f7091d9d27f8e82ca04
Milutin-S <studen.m98@gmail.com>
2023-05-18 20:43:25 +0200

    Update globals script with Paths and Const values

--- Digit_Recognizer/scripts/globals.py
+++ Digit_Recognizer/scripts/globals.py
@@ -1,10 +1,29 @@
 from pathlib import Path
 import torchvision.transforms as transforms
 
-TRAIN_PATH = Path('./Digit_Recognizer/Dataset/train.csv')
-TEST_PATH = Path('./Digit_Recognizer/Dataset/test.csv')
+# PATHS
+TRAIN_PATH = Path("./Digit_Recognizer/Dataset/train.csv")
+TEST_PATH = Path("./Digit_Recognizer/Dataset/test.csv")
+OUTPUT_DIR = Path("./Digit_Recognizer/Train_Output")
+MODELS_DIR = Path("./Digit_Recognizer/Train_Output/models")
+PARAMETERS_DIR = Path("./Digit_Recognizer/Train_Output/parameters")
+MODELS_DIR.mkdir(parents=True, exist_ok=True)
+PARAMETERS_DIR.mkdir(parents=True, exist_ok=True)
 
-transforms = transforms.Compose(
-    [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
-    # Actual mean and standard deviation of MNIST dataset
+# CONSTS
+BATCH_SIZE = 16
+LEARNING_RATE = 1e-3
+EPOCH_NUM = 100
+
+# TRAIN_TRANSFORMS = transforms.Compose(
+#     [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+# )
+
+# transforms = transforms.Compose(
+#     [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+#     # Actual mean and standard deviation of MNIST dataset
+# )
+
+TRAIN_TRANSFORMS = transforms.Compose(
+    [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
 )
