218cfd240bffac1141baf1e380158cf3095e7d68
Milutin-S <studen.m98@gmail.com>
2023-05-25 21:25:41 +0200

    Update parameters for best model and add comments for next test

--- Digit_Recognizer/scripts/globals.py
+++ Digit_Recognizer/scripts/globals.py
@@ -14,11 +14,11 @@ MODELS_DIR.mkdir(parents=True, exist_ok=True)
 PARAMETERS_DIR.mkdir(parents=True, exist_ok=True)
 
 # CONSTS
-BATCH_SIZE = 64
-LEARNING_RATE = 5e-4
+BATCH_SIZE = 32  # 64
+LEARNING_RATE = 1e-4  # 6e-5, 3e-5
 EPOCH_NUM = 100
 IMAGE_SHAPE = (28, 28)
-RAND_TRESHOLD_69 = 0.85
+RAND_TRESHOLD_69 = 0.8
 
 # TRAIN_TRANSFORMS = transforms.Compose(
 #     [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
@@ -37,38 +37,41 @@ TRAIN_GENERAL_TRANSFORMS = transforms.Compose(
     [
         # transforms.ToTensor(),
         # transforms.Normalize((0.5,), (0.5,)),
-        transforms.RandomApply(
-            [transforms.RandomAffine(degrees=15, translate=(0.08, 0.08), fill=True)],
-            p=0.3,  # 20 0.1 0.1
+        transforms.RandomChoice(
+            [
+                transforms.RandomApply(
+                    [transforms.RandomResizedCrop(size=28, scale=(0.8, 1.2))], p=0.5  #
+                ),
+                transforms.RandomApply(
+                    [
+                        transforms.RandomAffine(
+                            degrees=20, translate=(0.1, 0.1), fill=True
+                        )  # 10,
+                    ],
+                    p=0.5,
+                ),
+            ]
         ),
-        transforms.RandomApply(
-            [transforms.RandomResizedCrop(size=28, scale=(0.8, 1.2))], p=0.3
-        ),
-        transforms.RandomPerspective(distortion_scale=0.2, p=0.6),
-        transforms.RandomErasing(p=0.3, scale=(0.007, 0.007)),
+        transforms.RandomPerspective(distortion_scale=0.2, p=0.6),  # 0.4
+        transforms.RandomErasing(p=0.5, scale=(0.007, 0.007)),
     ]
 )
 TRAIN_0_TRANSFORM = transforms.Compose(
     [
-        transforms.RandomHorizontalFlip(p=0.3),
-        transforms.RandomVerticalFlip(p=0.3),
-        transforms.RandomApply([transforms.RandomRotation(degrees=180)], p=0.3),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomVerticalFlip(p=0.5),
+        transforms.RandomApply([transforms.RandomRotation(degrees=180)], p=0.5),  # 45
     ]
 )
-TRAIN_3_TRANSFORM = transforms.RandomVerticalFlip()
+TRAIN_3_TRANSFORM = transforms.RandomVerticalFlip(p=0.5)
 TRAIN_8_TRANSFORM = transforms.Compose(
     [
-        transforms.RandomHorizontalFlip(p=0.3),
-        transforms.RandomVerticalFlip(p=0.3),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomVerticalFlip(p=0.5),
     ]
 )
 TRAIN_69_SWITCH = transforms.Compose(
-    [
-        transforms.RandomApply(
-            [transforms.RandomHorizontalFlip(p=1), transforms.RandomVerticalFlip(p=1)],
-            p=0.3,
-        ),
-    ]
+    [transforms.RandomHorizontalFlip(p=1), transforms.RandomVerticalFlip(p=1)]
 )
 
 if __name__ == "__main__":
