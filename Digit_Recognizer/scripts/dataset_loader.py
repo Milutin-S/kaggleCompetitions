84fe049d345ffea787edef7618fd8c0e41d22578
Milutin-S <studen.m98@gmail.com>
2023-06-20 23:45:52 +0200

    Update dataset class

--- Digit_Recognizer/scripts/dataset_loader.py
+++ Digit_Recognizer/scripts/dataset_loader.py
@@ -3,20 +3,21 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
-from globals import (
-    TRAIN_PATH,
-    IMAGE_SHAPE,
-    TRAIN_GENERAL_TRANSFORMS,
-    TRAIN_0_TRANSFORM,
-    TRAIN_3_TRANSFORM,
-    TRAIN_8_TRANSFORM,
-    TRAIN_69_SWITCH,
-    RAND_TRESHOLD_69,
-    REQUIRED_TRANSFORMS,
-)
+from globals import *
 
 import torch
 from torch.utils.data import DataLoader, Dataset
+
+# #############
+# import importlib.util
+# import sys
+# spec = importlib.util.spec_from_file_location("globals", "C:\Users\Studen\Documents\vscode_projects\kaggleCompetitions\Digit_Recognizer\scripts\globals.py")
+# foo = importlib.util.module_from_spec(spec)
+# sys.modules["globals"] = foo
+# spec.loader.exec_module(foo)
+# foo.MyClass()
+# ###################
+
 
 # TODO:
 # Ideas
@@ -24,7 +25,9 @@ from torch.utils.data import DataLoader, Dataset
 
 
 class DigitDataset(Dataset):
-    def __init__(self, data_path: Path, transforms=None, type: str = "train") -> None:
+    def __init__(
+        self, data_path: Path, transforms: bool = False, type: str = "train"
+    ) -> None:
         super().__init__()
         self.dataset = pd.read_csv(data_path)
         self.transforms = transforms
@@ -37,26 +40,13 @@ class DigitDataset(Dataset):
         data = self.dataset.iloc[index]
         if self.type == "train":
             label, data = data[0], data[1:]
-        data = np.reshape(np.array(data, dtype=np.float32), IMAGE_SHAPE)
+        data = np.reshape(np.array(data, dtype=np.uint8), IMAGE_SHAPE)
         data = REQUIRED_TRANSFORMS(data)
+        # transform = transforms.ToTensor()
+        # data = transform(data)
 
         if self.transforms and self.type == "train":
-            if label == 0:
-                data = TRAIN_0_TRANSFORM(data)
-            elif label == 3:
-                data = TRAIN_3_TRANSFORM(data)
-            elif label == 8:
-                data = TRAIN_8_TRANSFORM(data)
-            elif (label == 6 or label == 9) and random.random() > RAND_TRESHOLD_69:
-                data = TRAIN_69_SWITCH(data)
-                if label == 6:
-                    label = 9
-                else:
-                    label = 6
-            data = self.transforms(data)
-
-        elif self.transforms and self.type == "test":
-            data = self.transforms(data)
+            data, label = apply_transforms(data, label)
 
         if self.type == "test":
             return data
@@ -73,11 +63,13 @@ def data_viz(data, label, tile: bool = False):
             img = data[i].squeeze()
             plt.subplot(plt_row, plt_col, i + 1)
             plt.title("Label: " + str(int(label[i])))
+            plt.axis("off")
             plt.imshow(img)
         plt.tight_layout()
     else:
         img = data.squeeze()
         plt.title("Label: " + str(int(label)))
+        plt.axis("off")
         plt.imshow(img)
     plt.show()
 
@@ -87,10 +79,13 @@ if __name__ == "__main__":
     # set_cwdb = 'c:\\Users\\Studen\\Documents\\vscode_projects\\kaggleCompetitions\\'
     # print(f"[INFO] Changing working directory to: {set_cwdb}")
     # os.chdir(set_cwdb)
-    tarin_set = DigitDataset(TRAIN_PATH, TRAIN_GENERAL_TRANSFORMS, type="train")
-    train_loader = DataLoader(tarin_set, batch_size=16, shuffle=False, num_workers=10)
+    train_set = DigitDataset(TRAIN_PATH, type="train")
+    train_loader = DataLoader(train_set, batch_size=16, shuffle=False, num_workers=10)
 
     images, labels = next(iter(train_loader))
+    print(
+        f"[INFO] Minimum data value: {torch.min(images[0])}, Maximum data value: {torch.max(images[0])}"
+    )
     print(images.shape)
     data_viz(data=images, label=labels, tile=True)
     # data_viz(data=images[0], label=labels[0])
